5c22d056163fcc02b18116b6ebf6d70119a42e90
NamanHS <namansanura@gmail.com>
2021-02-22 13:52:39 +0530

    home feature pages

--- app/src/main/java/tech/hans/glucotracker/enterReading.java
+++ app/src/main/java/tech/hans/glucotracker/enterReading.java
@@ -3,6 +3,9 @@ package tech.hans.glucotracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+
+import com.google.android.material.chip.ChipGroup;
 
 public class enterReading extends AppCompatActivity {
 
@@ -11,4 +14,6 @@ public class enterReading extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_reading);
     }
+
+
 }
